f70e534aa6d43a2a4f93205e327696fbf9ed3f7a
Dani Postigo <dani@firstperson.is>
2014-05-25 14:23:38 -0400

    -

--- DPAnimators/Utils/BasicAnimator+UIViewController.h
+++ DPAnimators/Utils/BasicAnimator+UIViewController.h
@@ -3,7 +3,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <DPTransitions/BasicAnimator.h>
+#import "BasicAnimator.h"
 
 @interface BasicAnimator (UIViewController)
 
--- DPAnimators/Utils/UIViewController+BasicAnimator.m
+++ DPAnimators/Utils/UIViewController+BasicAnimator.m
@@ -2,7 +2,7 @@
 // Created by Dani Postigo on 5/10/14.
 //
 
-#import <DPTransitions/BasicAnimator.h>
+#import "BasicAnimator.h"
 #import "UIViewController+BasicAnimator.h"
 #import "BasicAnimator+UIViewController.h"
 
